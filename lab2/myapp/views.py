767131fb80275ac1c4dfc8be358b70f432f1c645
julialiv09 <72882532+julialiv09@users.noreply.github.com>
2020-12-06 23:26:45 +0300

    Update views.py

--- lab2/myapp/views.py
+++ lab2/myapp/views.py
@@ -12,13 +12,7 @@ def index(request):
         if email == "Wrong password" or email == "User not found":
             return render(request, "bad.html")
         send_code(email)
-        # userform = UserForm2()
         return HttpResponseRedirect("input_code")
-        # return render(request, "auth.html", {"form": userform})
-
-        # name = request.POST.get("name")
-        # age = request.POST.get("age")     # получение значения поля age
-        # return HttpResponse("<h2>Hello, {0}</h2>".format(name))
     else:
         userform = UserForm()
         return render(request, "index.html", {"form": userform})
